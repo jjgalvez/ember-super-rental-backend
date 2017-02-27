4c1faea6a8035c2eb93f461a12f5539b47541500
Joes Galvez <jose@cybergalvez.com>
2017-02-26 21:01:05 -0500

    finished tutorial

--- superrental/api/base.py
+++ superrental/api/base.py
@@ -5,11 +5,15 @@ from flask.json import jsonify
 from flask import request
 
 @app.route('/api/rentals', methods=['GET'])
-def rentals():
-    if request.method == 'GET' and not request.values.get('city', False):
+@app.route('/api/rentals/<id>', methods=['GET'])
+def rentals(id=False):
+    if request.method == 'GET' \
+        and not request.values.get('city', False) \
+        and not id:
         rentals = [rental(x) for x in mongo.db.rentals.find()]
         return jsonify( dict(data=rentals) )
-    elif request.method == 'GET' and request.values.get('city'):
+    elif request.method == 'GET' and request.values.get('city') \
+        and not id:
         city = request.values.get('city').lower().strip()
         print(city)
         rentals = [rental(x) for x in mongo.db.rentals.find({
@@ -17,5 +21,10 @@ def rentals():
                                         '$options': 'i'}
                                 })]
         return jsonify(dict(data=rentals))
+    elif request.method == 'GET' and \
+        not request.values.get('city', False) and \
+        id:
+        rentalInfo = mongo.db.rentals.find_one_or_404(dict(id=id))
+        return jsonify(dict(data=rental(rentalInfo)))
     else:
         return dict(data=[])
